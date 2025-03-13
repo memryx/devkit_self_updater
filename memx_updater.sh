2105086caa8c78941e6b86ce3d0ec6b1f9337f3e
Tim <tim@memryx.com>
2025-03-13 16:50:42 -0400

    also rebuild C++ apps

--- memx_updater.sh
+++ memx_updater.sh
@@ -11,14 +11,14 @@ cd /home/memryx/.memx_updater
 git fetch origin
 reslog=$(git log HEAD..origin/main --oneline)
 if [[ "${reslog}" != "" ]]; then
-	printf "\033[92mUpdate found! Downloading and restarting myself...\033[0m\n"
-	# update available
-	git pull origin main
-	# restart myself
-	./memx_updater.sh
-	exit $?
+  printf "\033[92mUpdate found! Downloading and restarting myself...\033[0m\n"
+  # update available
+  git pull origin main
+  # restart myself
+  ./memx_updater.sh
+  exit $?
 else
-	echo -e "No script updates found.\n"
+  echo -e "No script updates found.\n"
 fi
 
 
@@ -32,13 +32,13 @@ read -n 1 -p " [Y]/N:  " pipcheck
 pipcheck=${pipcheck:-Y}
 
 if [[ $pipcheck == "Y" ]] || [[ $pipcheck == "y" ]]; then
-	. ~/mx/bin/activate &&
-	pip install --upgrade --extra-index-url https://developer.memryx.com/pip memryx
-	if [ $? -ne 0 ]; then
-		echo -e "\n\n\033[91mError\033[0m: something failed. Please see error logs above.\n"
-		read "Press ENTER to close this window..." junk
-		exit 1
-	fi
+  . ~/mx/bin/activate &&
+  pip install --upgrade --extra-index-url https://developer.memryx.com/pip memryx
+  if [ $? -ne 0 ]; then
+    echo -e "\n\n\033[91mError\033[0m: something failed. Please see error logs above.\n"
+    read "Press ENTER to close this window..." junk
+    exit 1
+  fi
 fi
 
 echo -e "\n\n"
@@ -53,13 +53,13 @@ read -n 1 -p " [Y]/N:  " aptcheck
 aptcheck=${aptcheck:-Y}
 
 if [[ $aptcheck == "Y" ]] || [[ $aptcheck == "y" ]]; then
-	sudo apt update &&
-	sudo apt install memx-drivers memx-accl memx-accl-plugins memx-accl-plugins-u24 memx-utils-gui memx-utils-gui-cv46
-	if [ $? -ne 0 ]; then
-		echo -e "\n\n\033[91mError\033[0m: something failed. Please see error logs above.\n"
-		read "Press ENTER to close this window..." junk
-		exit 1
-	fi
+  sudo apt update &&
+  sudo apt install memx-drivers memx-accl memx-accl-plugins memx-accl-plugins-u24 memx-utils-gui memx-utils-gui-cv46
+  if [ $? -ne 0 ]; then
+    echo -e "\n\n\033[91mError\033[0m: something failed. Please see error logs above.\n"
+    read "Press ENTER to close this window..." junk
+    exit 1
+  fi
 fi
 
 echo -e "\n\n"
@@ -69,18 +69,28 @@ echo -e "\n\n"
 # examples puller
 ###############################################################################
 
-printf "\033[96m'git pull' the latest examples in ~/memryx_examples?\033[0m " gitcheck
+printf "\033[96m'git pull' the latest examples in ~/memryx_examples and rebuild apps?\033[0m " gitcheck
 read -n 1 -p " [Y]/N:  " gitcheck
 gitcheck=${gitcheck:-Y}
 
 if [[ $gitcheck == "Y" ]] || [[ $gitcheck == "y" ]]; then
-	cd ~/memryx_examples &&
-	git pull
-	if [ $? -ne 0 ]; then
-		echo -e "\n\n\033[91mError\033[0m: something failed. Please see error logs above.\n"
-		read "Press ENTER to close this window..." junk
-		exit 1
-	fi
+  cd ~/memryx_examples &&
+  git pull
+  if [ $? -ne 0 ]; then
+    echo -e "\n\n\033[91mError\033[0m: something failed. Please see error logs above.\n"
+    read "Press ENTER to close this window..." junk
+    exit 1
+  fi
+
+  # DepthEstimation
+  cd /home/memryx/memryx_examples/video_inference/depth_midas/src/cpp/build/ && make
+
+  # YOLOv8m Segmentation
+  cd /home/memryx/memryx_examples/video_inference/segmentation_yolov8/src/cpp/build/ && make
+  
+  # YOLOv9s Video Obj. Det.
+  cd /home/memryx/memryx_examples/multistream_video_inference/object_detection_yolov8/src/cpp/build/ && make
+
 fi
 
 
